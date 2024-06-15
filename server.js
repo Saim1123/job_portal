8a17d060fac25d55215e765affd28fa53dfa4c75
SaimTahir <127545146+Saim1123@users.noreply.github.com>
2024-06-15 19:56:56 +0500

    Enhancing and Securing Node Server.

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
-const express = require("express");
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+
+dotenv.config();
 
 const app = express();
 
@@ -6,4 +10,10 @@ app.get("/", (req, res) => {
   res.send("<h1>Home Page</h1>");
 });
 
-app.listen(8080, () => console.log("Server is running..."));
+const port = process.env.PORT || 8080;
+
+app.listen(port, () =>
+  console.log(
+    `Server running in ${process.env.DEV_MODE} on port ${port}...`.bgCyan.white
+  )
+);
